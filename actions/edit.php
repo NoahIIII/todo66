6731728b5be47beaaaa1c4046a0379d928e5d114
NoahIIII <123589841+NoahIIII@users.noreply.github.com>
2023-08-24 19:40:44 +0300

    Update edit.php

--- actions/edit.php
+++ actions/edit.php
@@ -5,7 +5,17 @@ include_once '../helpers/functions.php';
 include_once '../helpers/validations.php';
 $errors=[];
 $task_id=$_POST['task_id'];
-$new_task=$_POST['new_task'];
+$new_task=sanitization($_POST['new_task']);
+if(TaskLength($new_task,$errors))
+{
 $sql = $con -> prepare("UPDATE TASKS SET TASK = '$new_task' where id = $task_id;");
 $sql->execute();
 header('location:../profile.php');
+die;
+}
+else{
+    $_SESSION["error"]=$errors;
+    header('location:../profile.php');
+    die;
+}
+
